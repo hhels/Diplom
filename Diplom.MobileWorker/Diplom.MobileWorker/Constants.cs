0600df75e24d43adbb7b811de29e8a40b9d829af
hhels <hels-hels@list.ru>
2020-05-21 23:48:40 +0300

    воркер

--- Diplom.MobileWorker/Diplom.MobileWorker/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Diplom.MobileWorker
-{
-    public class Constants
-    {
-        public const string Endpoint = "http://192.168.1.12:5002";
-        public const string EndpointImage = Endpoint + "/images/";
-    }
-}
